797a1fb54dd6838b39044d4b5bbd4c94e541c4e4
Roman <roman.ossif256@gmail.com>
2023-08-26 18:59:55 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace NotepadClone
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
